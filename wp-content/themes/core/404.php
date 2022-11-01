3f500766908d6acdf6d5d42537c23347457934fe
David Paul Ellenwood <davee@tri.be>
2022-11-01 12:50:11 -0500

    Drop php templates in favor of FSE.

--- wp-content/themes/core/404.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-get_header();
-?>
-	<main>
-		<h1>404</h1>
-	</main>
-<?php
-get_footer();
--- wp-content/themes/core/single.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-get_header(); ?>
-	<main>
-		<?php the_title( '<h1>', '</h1>', true ); ?>
-		<?php the_content(); ?>
-	</main>
-<?php
-get_footer();
