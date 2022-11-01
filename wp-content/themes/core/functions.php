d4b085fc3739662532565d5ec21cb55f04c35377
David Paul Ellenwood <davee@tri.be>
2022-11-01 13:03:36 -0500

    Update functions.php

--- wp-content/themes/core/functions.php
+++ wp-content/themes/core/functions.php
@@ -1,26 +1,5 @@
 <?php declare(strict_types=1);
 
-$tribe_autoloaders = [
-	// WP sub folder
-	trailingslashit( ABSPATH ) . '../vendor/autoload.php',
-	// WP standard
-	trailingslashit( ABSPATH ) . 'vendor/autoload.php',
-	// In the core plugin
-	trailingslashit( __DIR__ ) . 'vendor/autoload.php',
-];
-
-foreach ( $tribe_autoloaders as $autoloader ) {
-	if ( file_exists( $autoloader ) ) {
-		require_once $autoloader;
-		break;
-	}
-}
-
 /**
- * @TODO: Decide on an approach for this.
+ * Core theme functions
  */
-if ( ! function_exists( 'get_component_part' ) ) {
-	function get_component_part( string $slug, array $args = [] ): void {
-		get_template_part( 'components/' . $slug, '', $args );
-	}
-}
