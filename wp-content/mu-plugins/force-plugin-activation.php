c3627b693a40baf341dd616fb328af559fbd8218
Chris Kindred <cmkindred@gmail.com>
2023-02-22 15:44:26 -0500

    Update deploy exclude to include extra settings files, Remove unused plugin from force activation

--- wp-content/mu-plugins/force-plugin-activation.php
+++ wp-content/mu-plugins/force-plugin-activation.php
@@ -87,11 +87,6 @@ class Force_Plugin_Activation {
 			$this->force_active[]     = 'limit-login-attempts-reloaded/limit-login-attempts-reloaded.php';
 		}
 
-		// Specific config when unit tests are running
-		if ( defined( 'DIR_TESTDATA' ) && DIR_TESTDATA ) {
-			//$this->force_deactivate[] = 'term-sorter/term-sorter.php';
-		}
-
 		add_filter( 'option_active_plugins', [ $this, 'force_plugins' ], 10, 1 );
 		add_filter( 'site_option_active_sitewide_plugins', [ $this, 'force_plugins' ], 10, 1 );
 		add_filter( 'plugin_action_links', [ $this, 'plugin_action_links' ], 99, 2 );
