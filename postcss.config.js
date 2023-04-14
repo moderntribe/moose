febb4a8bc4cb983ef8c27cd26c90c7e4be1b529d
Jamie Paul <jamie@tri.be>
2023-04-14 17:54:16 -0500

    [MOOSE-8]: add postcss-inline-svg support

--- postcss.config.js
+++ postcss.config.js
@@ -8,6 +8,7 @@
  *  	- Reference: https://www.npmjs.com/package/postcss-import
  * - postcssGlobalData: Add support for the referenced set of css partials to be used globally,
  *  	without individually importing the partials in every file where they are used.
+ * - postcss-inline-svg: Add support for inline SVG icons.
  * - postcss-preset-env: Sets config to process all features (stage 0 and above)
  *  	- Adds autoprefixer support for css grid
  *  	- Removes any transformations on (don't modify) css custom properties, :focus-visible, and :focus-within
@@ -19,6 +20,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 const pkg = require( './package.json' );
 const { sync: glob } = require( 'fast-glob' );
 const postcssGlobalData = require( '@csstools/postcss-global-data' );
+const postcssInlineSvg = require( 'postcss-inline-svg' );
 
 /**
  * Replicates WP-Scripts config for CSS Nano.
@@ -44,6 +46,9 @@ const plugins = [
 		),
 	} ),
 	'postcss-mixins',
+	postcssInlineSvg( {
+		paths: [ `${ pkg.config.coreThemeDir }/assets/media/icons` ],
+	} ),
 	[
 		'postcss-preset-env',
 		{
