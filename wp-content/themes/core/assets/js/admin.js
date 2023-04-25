765bbba01743caa2df26b5fa395ce57c3ec2cd0e
Jamie Paul <jamie@tri.be>
2023-04-25 15:11:11 -0500

    [MOOSE-23]: unregister core quote block styles

--- wp-content/themes/core/assets/js/admin.js
+++ wp-content/themes/core/assets/js/admin.js
@@ -1,1 +1,12 @@
+/**
+ * unregister some block styles we don't need
+ */
+
+wp.domReady( () => {
+	if ( wp.blocks ) {
+		wp.blocks.unregisterBlockStyle( 'core/quote', 'default' );
+		wp.blocks.unregisterBlockStyle( 'core/quote', 'plain' );
+	}
+} );
+
 console.info( 'tribe-admin js loaded' );
