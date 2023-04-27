00237b2b9a10df2a0b693bc9abb4e187bd55b552
Jamie Paul <jamie@tri.be>
2023-04-27 07:32:04 -0500

    [MOOSE-23]: unregister separator block styles

--- wp-content/themes/core/assets/js/admin.js
+++ wp-content/themes/core/assets/js/admin.js
@@ -1,1 +1,13 @@
+/**
+ * unregister some block styles we don't need
+ */
+
+wp.domReady( () => {
+	if ( wp.blocks ) {
+		wp.blocks.unregisterBlockStyle( 'core/separator', 'default' );
+		wp.blocks.unregisterBlockStyle( 'core/separator', 'wide' );
+		wp.blocks.unregisterBlockStyle( 'core/separator', 'dots' );
+	}
+} );
+
 console.info( 'tribe-admin js loaded' );
