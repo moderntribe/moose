b5b849ab215e4232069041f8fb861cfd197caa94
Geoff Dusome <geoffd@tri.be>
2023-07-18 15:58:28 -0400

    [MOOSE-38]: post card cleanup / fixes

--- wp-content/themes/core/patterns/card-post-search-result.php
+++ wp-content/themes/core/patterns/card-post-search-result.php
@@ -13,19 +13,13 @@
 <!-- wp:query {"queryId":0,"query":{"perPage":"16","pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"taxQuery":null,"parents":[]},"displayLayout":{"type":"list","columns":3},"align":"wide"} -->
 <div class="wp-block-query alignwide"><!-- wp:post-template -->
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","right":"0","bottom":"var:preset|spacing|40","left":"0"}}},"className":"p-card-search-result l-clearfix","layout":{"type":"default"}} -->
-<div class="wp-block-group p-card-search-result l-clearfix" style="padding-top:var(--wp--preset--spacing--40);padding-right:0;padding-bottom:var(--wp--preset--spacing--40);padding-left:0"><!-- wp:post-featured-image {"width":"22%","align":"right","className":"s-aspect-ratio-cover s-aspect-ratio-4-3"} /-->
+<div class="wp-block-group p-card-search-result l-clearfix" style="padding-top:var(--wp--preset--spacing--40);padding-right:0;padding-bottom:var(--wp--preset--spacing--40);padding-left:0"><!-- wp:post-featured-image {"width":"22%","align":"right","className":"p-card-search-result__image s-aspect-ratio-cover s-aspect-ratio-4-3"} /-->
 
-<!-- wp:post-title {"style":{"spacing":{"margin":{"top":"0","bottom":"var:preset|spacing|10"}}},"fontSize":"60"} /-->
+<!-- wp:post-title {"style":{"spacing":{"margin":{"top":"0","bottom":"var:preset|spacing|10"}}},"className":"p-card-search-result__title","fontSize":"60"} /-->
 
 <!-- wp:post-excerpt /-->
 
 <!-- wp:post-title {"isLink":true,"className":"a-hidden-link-cover"} /--></div>
 <!-- /wp:group -->
-<!-- /wp:post-template -->
-
-<!-- wp:query-no-results -->
-<!-- wp:paragraph {"align":"left"} -->
-<p class="has-text-align-left">No Results</p>
-<!-- /wp:paragraph -->
-<!-- /wp:query-no-results --></div>
+<!-- /wp:post-template --></div>
 <!-- /wp:query -->
--- wp-content/themes/core/patterns/card-post.php
+++ wp-content/themes/core/patterns/card-post.php
@@ -12,20 +12,14 @@
 ?>
 <!-- wp:query {"queryId":0,"query":{"perPage":"18","pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"taxQuery":null,"parents":[]},"displayLayout":{"type":"flex","columns":3},"align":"wide"} -->
 <div class="wp-block-query alignwide"><!-- wp:post-template -->
-<!-- wp:group {"style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}},"className":"p-card-post l-clearfix","layout":{"type":"default"}} -->
-<div class="wp-block-group p-card-post l-clearfix" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:post-featured-image {"width":"100%","className":"s-aspect-ratio-cover s-aspect-ratio-4-3"} /-->
+<!-- wp:group {"tagName":"article","style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}},"className":"p-card-post l-clearfix","layout":{"type":"default"}} -->
+<article class="wp-block-group p-card-post l-clearfix" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:post-featured-image {"width":"100%","className":"p-card-post__image s-aspect-ratio-cover s-aspect-ratio-4-3"} /-->
 
-<!-- wp:post-terms {"term":"category","style":{"spacing":{"margin":{"bottom":"var:preset|spacing|10"}}}} /-->
+<!-- wp:post-terms {"term":"category","separator":"","style":{"spacing":{"margin":{"bottom":"var:preset|spacing|10"}}},"className":"p-card-post__categories"} /-->
 
-<!-- wp:post-title {"style":{"spacing":{"margin":{"top":"0","bottom":"var:preset|spacing|10"}}},"fontSize":"40"} /-->
+<!-- wp:post-title {"style":{"spacing":{"margin":{"top":"0","bottom":"var:preset|spacing|10"}}},"className":"p-card-post__title","fontSize":"40"} /-->
 
-<!-- wp:read-more {"className":"a-link-cover"} /--></div>
+<!-- wp:read-more {"className":"p-card-post__link a-link-cover"} /--></article>
 <!-- /wp:group -->
-<!-- /wp:post-template -->
-
-<!-- wp:query-no-results -->
-<!-- wp:paragraph {"align":"left"} -->
-<p class="has-text-align-left">No Results</p>
-<!-- /wp:paragraph -->
-<!-- /wp:query-no-results --></div>
+<!-- /wp:post-template --></div>
 <!-- /wp:query -->
