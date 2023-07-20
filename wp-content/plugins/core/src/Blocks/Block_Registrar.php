c7b82f5c203997c02fa553f5382dae8fc846cb3a
Geoff Dusome <geoffd@tri.be>
2023-07-20 13:12:08 -0400

    [MOOSE-46]: add @todo for BE to look into a way to make core custom blocks to work like ACF custom blocks

--- wp-content/plugins/core/src/Blocks/Block_Registrar.php
+++ wp-content/plugins/core/src/Blocks/Block_Registrar.php
@@ -9,6 +9,12 @@ class Block_Registrar {
 	public function register( string $block_name, string $blocks_dir = self::BLOCKS_DIR ): void {
 		$args = [];
 
+		/**
+		 * @todo BE to look into if we can edit this in a way where we can pull tribe/ custom blocks
+		 * as well instead of only ACF custom blocks.
+		 *
+		 * @see https://github.com/moderntribe/moose/pull/63#discussion_r1269701151
+		 */
 		if ( ! str_contains( $block_name, 'tribe' ) ) {
 			$args = [
 				'render_callback' => [ $this, 'render_template' ],
