58afdc3bb13202602bd0c2ca616947e374893fef
Geoff Dusome <geoffd@tri.be>
2023-07-20 09:35:10 -0400

    [MOOSE-47]: add post permalink block; update search result card with new blocks

--- wp-content/themes/core/patterns/card-post-search-result.php
+++ wp-content/themes/core/patterns/card-post-search-result.php
@@ -15,9 +15,13 @@
 <!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|40","right":"0","bottom":"var:preset|spacing|40","left":"0"}}},"className":"p-card-search-result l-clearfix","layout":{"type":"default"}} -->
 <div class="wp-block-group p-card-search-result l-clearfix" style="padding-top:var(--wp--preset--spacing--40);padding-right:0;padding-bottom:var(--wp--preset--spacing--40);padding-left:0"><!-- wp:post-featured-image {"width":"22%","align":"right","className":"p-card-search-result__image s-aspect-ratio-cover s-aspect-ratio-4-3"} /-->
 
+<!-- wp:tribe/post-type-name {"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|10"}}}} /-->
+
 <!-- wp:post-title {"style":{"spacing":{"margin":{"top":"0","bottom":"var:preset|spacing|10"}}},"className":"p-card-search-result__title","fontSize":"60"} /-->
 
 <!-- wp:post-excerpt /-->
+
+<!-- wp:tribe/post-permalink {"style":{"spacing":{"margin":{"top":"var:preset|spacing|20","right":"0","bottom":"0","left":"0"}}}} /-->
 
 <!-- wp:post-title {"isLink":true,"className":"a-hidden-link-cover"} /--></div>
 <!-- /wp:group -->
