932e43d5f214f30586345d9beac858637fee1a27
Geoff Dusome <geoffd@tri.be>
2023-08-16 13:07:23 -0400

    change button unregister from default to fill

--- wp-content/themes/core/blocks/core/button/editor.js
+++ wp-content/themes/core/blocks/core/button/editor.js
@@ -6,6 +6,6 @@ import { ready } from 'utils/events.js';
 import { unregisterBlockStyle } from '@wordpress/blocks';
 
 ready( () => {
-	unregisterBlockStyle( 'core/button', 'default' );
+	unregisterBlockStyle( 'core/button', 'fill' );
 	unregisterBlockStyle( 'core/button', 'outline' );
 } );
