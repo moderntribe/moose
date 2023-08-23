2b01dc32d4846bc85175ca724150339229cac83a
David Paul Ellenwood <davee@tri.be>
2023-08-23 16:46:35 -0500

    Try: fixes for 6.3:  - Remove reset.pcss from the block editor theme css file as it bleeds into the admin/editor interface and breaks the pattern preview iframes.  - Enqueue the block editor theme css using a normal wp_enqueue_style call so it gets added to the: inline post editor, iframed post editor, and iframed FSE editor.

--- wp-content/plugins/core/src/Assets/Editor_Assets_Enqueuer.php
+++ wp-content/plugins/core/src/Assets/Editor_Assets_Enqueuer.php
@@ -11,7 +11,15 @@ class Editor_Assets_Enqueuer extends Assets_Enqueuer {
 	public function register(): void {
 		$args = $this->get_asset_file_args( $this->assets_path . self::ASSETS_FILE );
 
-		add_editor_style( $this->assets_path_uri . self::EDITOR_FILE_NAME . '.css' );
+		// add_editor_style( $this->assets_path_uri . self::EDITOR_FILE_NAME . '.css' );
+
+		wp_enqueue_style(
+			self::EDITOR,
+			$this->assets_path_uri . self::EDITOR_FILE_NAME . '.css',
+			[],
+			$args['version'] ?? false,
+			'all',
+		);
 
 		wp_enqueue_script(
 			self::EDITOR,
