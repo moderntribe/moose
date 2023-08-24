4fd23d4fd9b823bd47091f5fb04ca33b981169fd
David Paul Ellenwood <davee@tri.be>
2023-08-23 20:27:08 -0500

    Remove unused commented out code.

--- wp-content/plugins/core/src/Assets/Editor_Assets_Enqueuer.php
+++ wp-content/plugins/core/src/Assets/Editor_Assets_Enqueuer.php
@@ -11,8 +11,6 @@ class Editor_Assets_Enqueuer extends Assets_Enqueuer {
 	public function register(): void {
 		$args = $this->get_asset_file_args( $this->assets_path . self::ASSETS_FILE );
 
-		// add_editor_style( $this->assets_path_uri . self::EDITOR_FILE_NAME . '.css' );
-
 		wp_enqueue_style(
 			self::EDITOR,
 			$this->assets_path_uri . self::EDITOR_FILE_NAME . '.css',
