cd6102f48b19bad487c906e97152d02ba85258cc
Geoff Dusome <geoffd@tri.be>
2023-10-17 19:31:11 -0400

    remove query post context

--- wp-content/themes/core/blocks/tribe/terms/edit.js
+++ wp-content/themes/core/blocks/tribe/terms/edit.js
@@ -34,18 +34,11 @@ export default function Edit( {
 } ) {
 	const blockProps = useBlockProps();
 	const { taxonomyToUse, onlyPrimaryTerm, hasLinks } = attributes;
-	const { postType, query } = context;
-	let contextPostType = '';
-
-	if ( query !== undefined ) {
-		contextPostType = query.postType;
-	} else {
-		contextPostType = postType;
-	}
+	const { postType } = context;
 
 	const taxonomies = useSelect( ( select ) =>
 		select( 'core' ).getTaxonomies( {
-			type: contextPostType,
+			type: postType,
 			per_page: -1,
 		} )
 	);
