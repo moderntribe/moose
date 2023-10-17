57e250f42f20cf62cbd34fa76a14a150a2af9d5d
Chris Kindred <cmkindred@gmail.com>
2023-10-17 18:04:09 -0400

    Check for rest_request and fake the term

--- wp-content/plugins/core/src/Blocks/Terms_Block.php
+++ wp-content/plugins/core/src/Blocks/Terms_Block.php
@@ -28,15 +28,36 @@ class Terms_Block {
 	 */
 	public function get_the_terms(): array {
 		if ( $this->only_primary_term ) {
-			$term = $this->get_primary_term( get_the_ID(), $this->taxonomy );
-			if ( $term ) {
-				$this->terms[] = $term;
+			$primary_term = $this->get_primary_term( get_the_ID(), $this->taxonomy );
+			if ( $primary_term ) {
+				$this->terms[] = $primary_term;
+
+				return $this->terms;
 			}
-		} else {
-			$terms = get_the_terms( get_the_ID(), $this->taxonomy );
-			if ( $terms && ! is_wp_error( $terms ) ) {
-				$this->terms = $terms;
-			}
+		}
+
+		$all_terms = get_the_terms( get_the_ID(), $this->taxonomy );
+		if ( $all_terms && ! is_wp_error( $all_terms ) ) {
+			$this->terms = $all_terms;
+
+			return $this->terms;
+		}
+
+		/**
+		 * If we are in a REST_REQUEST then we are using the editor and
+		 * need to fake a term so that the user can see where it will
+		 * show if it is added to the post.  This will not show on
+		 * the FE of the site.
+		 */
+		if ( defined( 'REST_REQUEST' ) && REST_REQUEST ) {
+			$fake_term = new \WP_Term( (object) [
+				'term_id'  => -1,
+				'name'     => esc_html__( 'Terms Display Here', 'tribe' ),
+				'slug'     => 'terms-display-here',
+				'taxonomy' => 'tag',
+			]);
+
+			$this->terms[] = $fake_term;
 		}
 
 		return $this->terms;
