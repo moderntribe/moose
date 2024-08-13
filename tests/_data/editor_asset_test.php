b4dc43e46e2fb14ae66ff930f125281448005e90
Mykhailo Los <mlkilderkin@gmail.com>
2024-08-13 16:19:08 +0300

    [MOOSE-114]: Add extra tests and stub for acceptance tests

---
+++ tests/_data/editor_asset_test.php
@@ -0,0 +1,2 @@
+<?php
+return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '024bef6fb3a0bc82cb17');
