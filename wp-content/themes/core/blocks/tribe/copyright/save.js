3f1a97dfed168c4f60d46d2ba3fa8fa94ecaf3b9
Vinny Listrani <vinny@tri.be>
2025-01-17 12:30:44 -0600

    [MOOSE-157]: add missing files back into this branch.

---
+++ wp-content/themes/core/blocks/tribe/copyright/save.js
@@ -0,0 +1,21 @@
+import { useBlockProps } from '@wordpress/block-editor';
+import { __ } from '@wordpress/i18n';
+export default function save( { attributes } ) {
+	const { showStartingYear, startingYear, copyrightText } = attributes;
+	const currentYear = new Date().getFullYear().toString();
+	const title = copyrightText || wp.data.select( 'core' ).getSite().title;
+
+	let displayDate;
+
+	if ( showStartingYear && startingYear ) {
+		displayDate = startingYear + '–' + currentYear;
+	} else {
+		displayDate = currentYear;
+	}
+
+	return (
+		<p { ...useBlockProps.save() }>
+			{ __( 'Copyright', 'tribe' ) } © { displayDate } { title }
+		</p>
+	);
+}
