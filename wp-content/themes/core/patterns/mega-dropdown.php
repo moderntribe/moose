27227166d0b56c02f82808f941021a10d0d95653
Vinny Listrani <vinny@tri.be>
2025-07-09 12:12:28 -0500

    [MOOSE-156]: update all template parts and patterns.

--- wp-content/themes/core/patterns/mega-dropdown.php
+++ wp-content/themes/core/patterns/mega-dropdown.php
@@ -8,11 +8,16 @@
 * Keywords: menu, navigation, footer, mega nav
 */
 ?>
-<!-- wp:group {"metadata":{"categories":["navigation","menu"],"patternName":"patterns/mega-dropdown","name":"Mega Dropdown"},"align":"wide","className":"nav-menu__enhanced alignwide","style":{"spacing":{"blockGap":"var:preset|spacing|40","padding":{"right":"0","left":"0"}}},"layout":{"type":"default"}} -->
-<div class="wp-block-group nav-menu__enhanced alignwide" style="padding-right:0;padding-left:0"><!-- wp:group {"metadata":{"name":"Mega Dropdown Card"},"style":{"spacing":{"blockGap":"var:preset|spacing|30"},"layout":{"selfStretch":"fixed","flexSize":"400px"}},"layout":{"type":"flex","orientation":"vertical"}} -->
-<div class="wp-block-group"><!-- wp:image {"sizeSlug":"large","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
+<!-- wp:group {"metadata":{"categories":["navigation","menu"],"patternName":"patterns/mega-dropdown","name":"Mega Dropdown"},"align":"wide","className":"mega-menu-item__dropdown alignwide","style":{"spacing":{"blockGap":"var:preset|spacing|40","padding":{"right":"0","left":"0"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group mega-menu-item__dropdown alignwide" style="padding-right:0;padding-left:0"><!-- wp:group {"metadata":{"name":"Mega Dropdown Card"},"className":"mega-menu-item__dropdown-card","style":{"spacing":{"blockGap":"var:preset|spacing|30"},"layout":{"selfStretch":"fixed","flexSize":"400px"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group mega-menu-item__dropdown-card"><!-- wp:image {"sizeSlug":"large","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
 <figure class="wp-block-image size-large" style="margin-top:0;margin-bottom:0"><img src="https://placehold.co/184" alt=""/></figure>
 <!-- /wp:image -->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:heading {"level":3,"className":"is-style-x-small"} -->
+<h3 class="wp-block-heading is-style-x-small">Section Heading</h3>
+<!-- /wp:heading -->
 
 <!-- wp:paragraph {"className":"is-style-small","style":{"spacing":{"margin":{"top":"0"}}}} -->
 <p class="is-style-small" style="margin-top:0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel ante nec diam placerat vulputate. Fusce ut dui sagittis lacus rutrum tristique ut vitae leo.</p>
@@ -23,6 +28,7 @@
 <div class="wp-block-button is-style-ghost tribe-button-has-icon"><a class="wp-block-button__link wp-element-button" href="#">Menu Item 1 CTA</a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
 <!-- /wp:group -->
 
 <!-- wp:columns {"isStackedOnMobile":false,"metadata":{"name":"Mega Dropdown Columns"},"align":"full","className":"alignfull","style":{"layout":{"selfStretch":"fill","flexSize":null},"spacing":{"blockGap":{"top":"0","left":"var:preset|spacing|50"},"margin":{"top":"0","bottom":"0"}}}} -->
