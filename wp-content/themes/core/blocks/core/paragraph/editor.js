1f1c6121a123dc489f1bcbfec0857cbd8cb5ad64
Saulo Padilha <saulo@tri.be>
2025-07-24 00:35:45 -0300

    [MOOSE-243]: Add heading and paragraph text balance settings

---
+++ wp-content/themes/core/blocks/core/paragraph/editor.js
@@ -0,0 +1,26 @@
+import { __ } from '@wordpress/i18n';
+import createWPControls from 'utils/create-wp-controls';
+
+const settings = {
+	attributes: {
+		useBalancedText: {
+			type: 'boolean',
+		},
+	},
+	blocks: [ 'core/paragraph' ],
+	controls: [
+		{
+			applyClass: 'has-balanced-text',
+			attribute: 'useBalancedText',
+			defaultValue: false,
+			helpText: __(
+				'Enable to evenly balance text over multiple lines',
+				'tribe'
+			),
+			label: __( 'Use Balanced Text', 'tribe' ),
+			type: 'toggle',
+		},
+	],
+};
+
+createWPControls( settings );
