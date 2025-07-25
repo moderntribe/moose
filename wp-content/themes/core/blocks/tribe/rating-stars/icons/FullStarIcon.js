a44185bec40195f85a611fc105b89c5a7f588158
Saulo Padilha <saulo@tri.be>
2025-07-25 02:49:58 -0300

    [MOOSE-239]: Fix icons a11y

--- wp-content/themes/core/blocks/tribe/rating-stars/icons/FullStarIcon.js
+++ wp-content/themes/core/blocks/tribe/rating-stars/icons/FullStarIcon.js
@@ -7,6 +7,8 @@ export default function FullStarIcon() {
 			fill="none"
 			xmlns="http://www.w3.org/2000/svg"
 			xmlSpace="preserve"
+			role="presentation"
+			aria-hidden="true"
 		>
 			<path
 				d="M32 5.37201L40.652 22.9L60 25.712L46 39.358L49.304 58.628L32 49.534L14.696 58.628L18 39.358L4 25.712L23.348 22.9L32 5.37201Z"
