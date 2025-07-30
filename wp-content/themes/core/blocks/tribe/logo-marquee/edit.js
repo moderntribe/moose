c63c3397693a706fe814773cd908384488da1ab4
Saulo Padilha <saulo@tri.be>
2025-07-30 02:57:39 -0300

    [MOOSE-248]: Add drag-and-drop reordering functionality using dnd-kit

--- wp-content/themes/core/blocks/tribe/logo-marquee/edit.js
+++ wp-content/themes/core/blocks/tribe/logo-marquee/edit.js
@@ -9,6 +9,80 @@ import { __ } from '@wordpress/i18n';
 import { trash } from '@wordpress/icons';
 import './editor.pcss';
 
+import {
+	DndContext,
+	closestCenter,
+	PointerSensor,
+	useSensor,
+	useSensors,
+} from '@dnd-kit/core';
+import { SortableContext, useSortable, arrayMove } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
+import { useCallback } from '@wordpress/element';
+
+// Sortable item component
+const SortableImage = ( {
+	id,
+	img,
+	index,
+	isSelected,
+	removeImage,
+	imageStyle,
+} ) => {
+	const { attributes, listeners, setNodeRef, transform, transition } =
+		useSortable( { id } );
+
+	const style = {
+		transform: CSS.Transform.toString( transform ),
+		transition,
+		cursor: 'move',
+		position: 'relative',
+	};
+
+	return (
+		<div
+			ref={ setNodeRef }
+			{ ...attributes }
+			style={ style }
+			className="gallery-item"
+		>
+			<div
+				{ ...attributes }
+				{ ...listeners }
+				style={ {
+					cursor: 'grab',
+				} }
+				aria-label="Drag to reorder"
+			>
+				<img
+					src={ img.url }
+					alt={ img.alt }
+					data-id={ img.id }
+					style={ imageStyle }
+				/>
+			</div>
+			{ isSelected && (
+				<Button
+					className="remove-image-button"
+					icon={ trash }
+					aria-label={ __( 'Remove image' ) }
+					onClick={ () => removeImage( index ) }
+					style={ {
+						position: 'absolute',
+						top: '-15px',
+						right: '-30px',
+						backgroundColor: 'rgba(0,0,0,0.7)',
+						color: 'white',
+						padding: '0px',
+						minWidth: '0',
+						height: 'auto',
+					} }
+				/>
+			) }
+		</div>
+	);
+};
+
 export default function Edit( { attributes, setAttributes, isSelected } ) {
 	const { images } = attributes;
 	const blockProps = useBlockProps();
@@ -31,11 +105,32 @@ export default function Edit( { attributes, setAttributes, isSelected } ) {
 		setAttributes( { images: newImages } );
 	};
 
+	// Handle drag-and-drop sort event
+	const handleDragEnd = useCallback(
+		( event ) => {
+			const { active, over } = event;
+			if ( active.id !== over?.id ) {
+				const oldIndex = images.findIndex(
+					( img ) => img.id === active.id
+				);
+				const newIndex = images.findIndex(
+					( img ) => img.id === over.id
+				);
+				setAttributes( {
+					images: arrayMove( images, oldIndex, newIndex ),
+				} );
+			}
+		},
+		[ images, setAttributes ]
+	);
+
 	// Basic image styles
 	const imageStyle = {
 		maxWidth: '200px',
 		height: 'auto',
 	};
+
+	const sensors = useSensors( useSensor( PointerSensor ) );
 
 	return (
 		<>
@@ -68,82 +163,74 @@ export default function Edit( { attributes, setAttributes, isSelected } ) {
 						multiple
 					/>
 				) : (
-					<div className="logo-list">
-						{ images.map( ( img, index ) => (
-							<div key={ index } className="gallery-item">
-								<div style={ { position: 'relative' } }>
-									<img
-										src={ img.url }
-										alt={ img.alt }
-										data-id={ img.id }
-										style={ imageStyle }
+					<DndContext
+						sensors={ sensors }
+						collisionDetection={ closestCenter }
+						onDragEnd={ handleDragEnd }
+					>
+						<SortableContext
+							items={ images.map( ( img ) => img.id ) }
+						>
+							<div className="logo-list">
+								{ images.map( ( img, index ) => (
+									<SortableImage
+										key={ img.id }
+										id={ img.id }
+										img={ img }
+										index={ index }
+										isSelected={ isSelected }
+										removeImage={ removeImage }
+										imageStyle={ imageStyle }
 									/>
-									{ isSelected && (
-										<Button
-											className="remove-image-button"
-											icon={ trash }
-											onClick={ () =>
-												removeImage( index )
-											}
-											style={ {
-												position: 'absolute',
-												top: '5px',
-												right: '5px',
-												backgroundColor:
-													'rgba(0,0,0,0.7)',
-												color: 'white',
-												padding: '2px',
+								) ) }
+								{ isSelected && (
+									<div
+										className="logo-list-add-item"
+										style={ {
+											display: 'flex',
+											justifyContent: 'center',
+											alignItems: 'center',
+											width: '200px',
+											height: '160px',
+											padding: '0 25px',
+											overflow: 'hidden',
+										} }
+									>
+										<MediaPlaceholder
+											icon="plus"
+											labels={ {
+												title: __( 'Add to gallery' ),
+												instructions: '',
 											} }
+											onSelect={ ( newImages ) => {
+												setAttributes( {
+													images: [
+														...images,
+														...newImages.map(
+															( image ) => ( {
+																id: image.id,
+																url:
+																	image.url ||
+																	image.source_url,
+																alt:
+																	image.alt ||
+																	image.alt_text ||
+																	'',
+															} )
+														),
+													],
+												} );
+											} }
+											accept="image/*"
+											allowedTypes={ [ 'image' ] }
+											multiple
+											value={ {} }
 										/>
-									) }
-								</div>
+									</div>
+								) }
 							</div>
-						) ) }
-						{ isSelected && (
-							<div
-								className="logo-list-add-item"
-								style={ {
-									border: '1px dashed #ddd',
-									display: 'flex',
-									justifyContent: 'center',
-									alignItems: 'center',
-									width: '200px',
-									height: '200px',
-								} }
-							>
-								<MediaPlaceholder
-									icon="plus"
-									labels={ {
-										title: __( 'Add to gallery' ),
-										instructions: '',
-									} }
-									onSelect={ ( newImages ) => {
-										setAttributes( {
-											images: [
-												...images,
-												...newImages.map(
-													( image ) => ( {
-														id: image.id,
-														url:
-															image.url ||
-															image.source_url,
-														alt:
-															image.alt ||
-															image.alt_text ||
-															'',
-													} )
-												),
-											],
-										} );
-									} }
-									accept="image/*"
-									allowedTypes={ [ 'image' ] }
-									multiple
-									value={ {} }
-								/>
-							</div>
-						) }
-					</div>
+						</SortableContext>
+					</DndContext>
 				) }
 			</div>
 		</>
