79eaf8e7c4daec99b9657b980aced1dad21d194e
Geoff Dusome <geoffd@tri.be>
2025-08-01 10:26:28 -0400

    [MOOSE-257]: test linting on commit

--- wp-config.php
+++ wp-config.php
@@ -7,9 +7,11 @@ require __DIR__ . '/wp-config-environment.php';
 // but not integration tests
 
 foreach ( [ 'DB_NAME', 'DB_USER', 'DB_PASSWORD', 'DB_HOST' ] as $dbvar ) {
+
 	if ( ! defined( $dbvar ) ) {
 		define( $dbvar, tribe_getenv( $dbvar, '' ) );
 	}
+    
 }
 
 if ( ! defined( 'ABSPATH' ) ) {
