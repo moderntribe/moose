956ace163d64dd85d9daecaca80535604bd330d9
Geoff Dusome <geoffd@tri.be>
2025-08-28 10:21:25 -0400

    [MOOSE-263]: initial block creation

---
+++ wp-content/themes/core/blocks/tribe/announcements/render.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+/**
+ * @var array $attributes
+ */
+?>
+<section <?php echo get_block_wrapper_attributes(); ?>>
+	<p><?php esc_html_e( 'announcements – hello from a dynamic block!', 'tribe' ); ?></p>
+</section>
