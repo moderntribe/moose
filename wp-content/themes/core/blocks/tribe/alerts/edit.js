2a209136b4fdb2946fb76462ceae3fac1521a32c
Jonathan Allen <jda@burnmedia.co.uk>
2025-09-18 08:56:05 +0100

    Tribe Alert Block

--- wp-content/themes/core/blocks/tribe/alerts/edit.js
+++ wp-content/themes/core/blocks/tribe/alerts/edit.js
@@ -1,39 +1,14 @@
-import { __ } from '@wordpress/i18n';
-import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
-import { PanelBody, TextControl } from '@wordpress/components';
+import { useBlockProps } from '@wordpress/block-editor';
+import ServerSideRender from '@wordpress/server-side-render';
 
 import './editor.pcss';
 
-export default function Edit( { attributes, setAttributes, isSelected } ) {
+export default function Edit() {
 	const blockProps = useBlockProps();
-
-	const { exampleTextControl } = attributes;
 
 	return (
 		<div { ...blockProps }>
-			<p>{ __( 'Alert – hello from the editor!', 'tribe' ) }</p>
-			{ exampleTextControl ? <p>{ exampleTextControl }</p> : '' }
-			{ isSelected && (
-				<InspectorControls>
-					<PanelBody title={ __( 'Block Settings', 'tribe' ) }>
-						<TextControl
-							label={ __( 'Example Text Control', 'tribe' ) }
-							value={ exampleTextControl }
-							help={ __(
-								'Some helpful text describing the text control.',
-								'tribe'
-							) }
-							placeholder={ __(
-								'Helpful placeholder text',
-								'tribe'
-							) }
-							onChange={ ( value ) =>
-								setAttributes( { exampleTextControl: value } )
-							}
-						/>
-					</PanelBody>
-				</InspectorControls>
-			) }
+			<ServerSideRender block="tribe/announcements" />
 		</div>
 	);
 }
--- wp-content/themes/core/blocks/tribe/alerts/render.php
+++ wp-content/themes/core/blocks/tribe/alerts/render.php
@@ -1,3 +1,12 @@
 <?php declare(strict_types=1);
 
-echo 'Alerts';
+use Tribe\Plugin\Components\Alert\Alert_Controller;
+use Tribe\Plugin\Components\Alert\Alert_Renderer;
+use Tribe\Plugin\Object_Meta\Post_Types\Alert_Meta;
+
+$placement = $attributes['placement'] ?? Alert_Meta::PLACEMENT_ABOVE;
+
+$controller = Alert_Controller::factory();
+$renderer = new Alert_Renderer( $controller );
+
+echo $renderer->parse_and_render( $placement );
