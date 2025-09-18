32f16c9f97ea70ec3442366890016bdaa01bf891
Jonathan Allen <jda@burnmedia.co.uk>
2025-09-18 11:28:57 +0100

    Rename to Announcement

--- wp-content/themes/core/blocks/tribe/announcement-renderer/index.js
+++ wp-content/themes/core/blocks/tribe/announcement-renderer/index.js
@@ -3,7 +3,6 @@ import { registerBlockType } from '@wordpress/blocks';
 import './style.pcss';
 
 import Edit from './edit';
-import save from './save';
 import metadata from './block.json';
 
 registerBlockType( metadata.name, {
