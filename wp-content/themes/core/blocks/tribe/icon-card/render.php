57d6e624eb72489db1ac9dbac8994a06a0be8aba
Geoff Dusome <geoffd@tri.be>
2025-09-29 10:39:57 -0400

    [MOOSE-260]: migrate icon picker logic to class for reusability

--- wp-content/themes/core/blocks/tribe/icon-card/render.php
+++ wp-content/themes/core/blocks/tribe/icon-card/render.php
@@ -1,55 +1,23 @@
 <?php declare(strict_types=1);
 
 use Tribe\Plugin\Blocks\Helpers\Block_Animation_Attributes;
+use Tribe\Plugin\Blocks\Helpers\Icon_Picker;
 
 /**
  * @var array $attributes
  */
 
 $animation_attributes  = new Block_Animation_Attributes( $attributes );
+$icon_picker           = new Icon_Picker( $attributes );
+$icon_wrapper_styles   = $icon_picker->get_icon_wrapper_styles();
+$icon_svg              = $icon_picker->get_svg();
 $classes               = 'b-icon-card';
-$icon_key              = $attributes['selectedIcon'] ?? '';
-$icon_color            = $attributes['selectedIconColor'] ?? 'currentColor';
-$bg_color              = $attributes['selectedBgColor'] ?? 'transparent';
-$padding               = intval( $attributes['iconPadding'] ?? 0 );
-$size                  = intval( $attributes['iconSize'] ?? 24 );
-$is_rounded            = ! empty( $attributes['isRounded'] );
-$icon_label            = $attributes['iconLabel'] ?? '';
 $title                 = $attributes['title'] ?: '';
 $description           = $attributes['description'] ?: '';
 $link_url              = $attributes['linkUrl'] ?: '';
 $link_opens_in_new_tab = $attributes['linkOpensInNewTab'] ?: false;
 $link_text             = $attributes['linkText'] ?: '';
 $link_a11y_label       = $attributes['linkA11yLabel'] ?: '';
-
-$style = sprintf(
-	'--icon-picker--background-color:%s;
-	--icon-picker--icon-color:%s;
-	--icon-picker--icon-size:%dpx;
-	--icon-picker--icon-padding:%dpx;
-	--icon-picker--border-radius:%s;',
-	esc_attr( $bg_color ),
-	esc_attr( $icon_color ),
-	$size,
-	$padding,
-	$is_rounded ? '50%' : '0'
-);
-
-$icon_path = get_template_directory() . '/blocks/tribe/icon-picker/icons/svg/' . $icon_key . '.svg';
-$svg       = '';
-
-if ( file_exists( $icon_path ) ) {
-	$svg = file_get_contents( $icon_path );
-
-	if ( $icon_label ) {
-		$svg = preg_replace(
-			'/<svg\b([^>]*)>/',
-			'<svg$1 aria-label="' . esc_attr( $icon_label ) . '" role="img">',
-			$svg,
-			1
-		);
-	}
-}
 
 if ( $animation_attributes->get_classes() !== '' ) {
 	$classes .= ' ' . $animation_attributes->get_classes();
@@ -58,10 +26,10 @@ if ( $animation_attributes->get_classes() !== '' ) {
 <article <?php echo get_block_wrapper_attributes( [ 'class' => esc_attr( $classes ), 'style' => $animation_attributes->get_styles() ] ); ?>>
 	<div class="b-icon-card__inner">
 		<div class="b-icon-card__top">
-			<?php if ( ! empty( $svg ) ) : ?>
+			<?php if ( ! empty( $icon_svg ) ) : ?>
 				<div class="b-icon-card__media">
-					<div class="b-icon-card__icon-wrapper" style="<?php echo esc_attr( $style ); ?>">
-						<?php echo $svg; ?>
+					<div class="b-icon-card__icon-wrapper" style="<?php echo esc_attr( $icon_wrapper_styles ); ?>">
+						<?php echo $icon_svg; ?>
 					</div>
 				</div>
 			<?php endif; ?>
