1b540d4b19ca7d07e40e1bd82bd43bc8a756eea0
Geoff Dusome <geoffd@tri.be>
2025-10-17 10:45:17 -0400

    reorder admin menu; cursor for dismiss button

--- wp-content/plugins/core/src/Menus/Admin_Menu_Order.php
+++ wp-content/plugins/core/src/Menus/Admin_Menu_Order.php
@@ -2,17 +2,21 @@
 
 namespace Tribe\Plugin\Menus;
 
+use Tribe\Plugin\Post_Types\Announcement\Announcement;
+use Tribe\Plugin\Post_Types\Page\Page;
+
 class Admin_Menu_Order {
 
 	public function custom_menu_order(): array {
 		return [
 			'index.php',
 			'separator1',
-			'edit.php?post_type=page',
+			'edit.php?post_type=' . Page::NAME,
 			// place CPTs here
 			'edit.php',
 			'upload.php',
 			'gf_edit_forms',
+			'edit.php?post_type=' . Announcement::NAME,
 			'separator2',
 			'themes.php',
 			'plugins.php',
